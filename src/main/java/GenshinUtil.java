f127fa8613d2feb600bac58988c427cd3c6c64ef
Aye10032 <aye10032@gmail.com>
2022-10-26 16:13:46 +0800

    init

---
+++ src/main/java/GenshinUtil.java
@@ -0,0 +1,13 @@
+/**
+ * @program: genshinutil
+ * @className: GenshinUtil
+ * @Description: 主类
+ * @version: v1.0
+ * @author: Aye10032
+ * @date: 2022/10/26 下午 4:13
+ */
+public class GenshinUtil {
+
+    public GenshinUtil(){}
+
+}
